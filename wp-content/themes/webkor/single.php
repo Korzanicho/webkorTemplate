f76c2967fa453890af7b611187a09d06d2342e8f
Korzanicho <adrianyamakasi@wp.pl>
2022-02-14 17:57:42 +0100

    (25): media

--- wp-content/themes/webkor/single.php
+++ wp-content/themes/webkor/single.php
@@ -9,6 +9,21 @@
                     <?php set_post_format( $post->ID, 'video' ); get_template_part( 'template-parts/content', get_post_format() ); ?>
                 </article>
             <?php
+
+                $args = [
+                    'post_parent'   => get_the_ID(),
+                    'post_type'     => 'attachment'
+                ];
+
+                $attachments = get_posts($args);
+
+                if ($attachments) {
+                    foreach ($attachments as $attachment) {
+                        $metaData = wp_get_attachment_metadata($attachment->ID);
+                        var_dump($metaData);
+                    }
+                }
+
                 wp_link_pages();
                 previous_post_link();
                 next_post_link();
